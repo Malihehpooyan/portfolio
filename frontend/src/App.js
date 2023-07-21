1f385c3c189efa3270795f5a0293c242f50f7325
malihehpooyan <malihehpooyan@gmail.com>
2023-07-21 11:16:37 -0400

    footer set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
-
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+
 
 function App() {
   return <Router>
+         
+          <Header />
           <Routes>
-            
-            <Route path="/" element={} />
+            <Route path="/" element={<Home />} />
           </Routes>
     </Router>
 
---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,34 @@
+import { Typography } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+import { BsGithub, BsYoutube, BsInstagram, BsLinkedin } from "react-icons/bs";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div>
+        <Typography variant="h5">About Me</Typography>
+        <Typography>
+          Hey, my name is Maliheh Zarei Chitgar. I am a entry level Full-Stack Developer. 
+        </Typography>
+
+        <Link to="/contact" className="footerContactBtn">
+          <Typography>Contact Me</Typography>
+        </Link>
+      </div>
+      <div>
+        <Typography variant="h6">Social Media</Typography>
+        <a href="https://github.com/malihehpooyan/" target="black">
+          <BsGithub />
+        </a>
+    
+        <a href="https://www.linkedin.com/in/malihehzarei/" target="black">
+          <BsLinkedin />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
